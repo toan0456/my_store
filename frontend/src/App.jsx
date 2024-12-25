e7470b3e424a3c06b71fa514bb9cc1df624f7b75
toan0456 <toanpdph42407@fpt.edu.vn>
2024-12-25 15:31:15 +0700

    fe - build react-vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
+import Navbar from "./components/Navbar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+        {/* Background gradient */}
+			<div className='absolute inset-0 overflow-hidden'>
+				<div className='absolute inset-0'>
+					<div className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]' />
+				</div>
+        </div>
+        <div className="relative z-50 pt-20" >
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+        </div >
+      </div>
+    </>
+  );
+}
+
+export default App;
